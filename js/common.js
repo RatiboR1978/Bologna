b4e791f7855a9795c3f08dd9bc75bef0ea5972e8
RatiboR1978 <klov29@mail.ru>
2017-08-10 15:05:11 +0700

    Сделал скролл и фиксацию меню

--- js/common.js
+++ js/common.js
@@ -1,7 +1,30 @@
 $(function () {
+    /* Скролл до инстограмма
+     =========================*/
 
+    $('.page-header__my-life').on('click', function () {
+        var instagramOffset = $('.instagram').offset().top;
 
+        $('html, body').animate({
+            scrollTop: instagramOffset
+        }, 500)
+    })
 
+    /* Фиксация меню
+     =========================*/
+
+    var headerH = $('.page-header').height(),
+        navH = $('.page-header__wrap').innerHeight();
+    $(document).on('scroll', function () {
+        var documentScroll = $(this).scrollTop();
+        if (documentScroll > headerH) {
+            $('.page-header__wrap').addClass('page-header__wrap-fixed');
+            $('.page-header').css('paddingTop', navH);
+        } else {
+            $('.page-header__wrap').removeClass('page-header__wrap-fixed');
+            $('.page-header').removeAttr('style');
+        }
+    })
 
 
 
