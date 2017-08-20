446aa53728c9a86c6a0430056b09ebabfd0e1d57
RatiboR1978 <klov29@mail.ru>
2017-08-20 07:25:37 +0700

    Переделал сетку

--- js/common.js
+++ js/common.js
@@ -26,6 +26,74 @@ $(function () {
         }
     })
 
+    /* Модальное окно
+     =========================*/
+    var widthModal = $('.modal').innerWidth()/2;
+    $('.modal').css({
+        'marginLeft': '-' + widthModal + 'px'
+    });
+    $('#contact').on('click', function (e) {
+        e.preventDefault();
+        $('.modal').fadeIn();
+        $('body').append('<div class="overlay"></div>').addClass('open-modal') //Скрываем скролл
 
+    });
+    $('.btn_red, .overlay').on('click', function (e) {
+        e.preventDefault();
+        $('.modal').fadeOut();
+        $('body').removeClass('open-modal') //Проявляем скролл
+        $('.overlay').remove();
+    });
+    $('body').on('click', '.overlay', function () {
+        $('.modal').fadeOut();
+        $('body').removeClass('open-modal') //Проявляем скролл
+        $('.overlay').remove();
+    });
 
+    /* Аккардион
+     =========================*/
+    /*$('.faq-title').on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        if(!$this.hasClass('active')) {
+            $('.faq-content').slideUp();
+            $this.removeClass('active')
+        }
+        $this.toggleClass('active');
+        $this.next().slideToggle(500)
+    })*/
+
+    $('.faq-title').on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this),
+            answerId = $this.attr('href');
+
+        if(!$this.hasClass('active')) {
+            $('.faq-content').slideUp();
+            $this.removeClass('active')
+        }
+        $this.toggleClass('active');
+        $(answerId).slideToggle(500)
+    });
+
+    /* Всплывающая подсказка
+     =========================*/
+    $('.popup-linck').hover(function () {
+        var $this = $(this),
+            popupId = $this.attr('href');
+        $(popupId).fadeIn();
+    }, function () {
+        $('.popup').fadeOut();
+    })
+
+    /* Masonry
+     =========================*/
+
+    $('.page-content__wrapper').masonry({
+        itemSelector: '.page-content__block',
+        columnWidth: 400,
+        gutter: 40
+
+    });
+    
 });
